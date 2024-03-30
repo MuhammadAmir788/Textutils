5cf6fd5f99e212ee63ae271a4d78926188561405
Muhammad Amir <164983564+MuhammadAmir788@users.noreply.github.com>
2024-03-30 12:34:40 +0500

    Add files via upload

---
+++ build/precache-manifest.a15d4737c4e8a825e3b3d14a4a25db88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2755034b8c3fb753f5b9d12b12da2355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f3b60c786fd5bf06ad",
+    "url": "/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "e01ea1eb178e0214f065",
+    "url": "/static/js/2.8899aa48.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/static/js/2.8899aa48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5770b6dbafba458b69",
+    "url": "/static/js/3.9d25297e.chunk.js"
+  },
+  {
+    "revision": "39f3b60c786fd5bf06ad",
+    "url": "/static/js/main.8130aeaf.chunk.js"
+  },
+  {
+    "revision": "c317c166b358f6acb1e5",
+    "url": "/static/js/runtime-main.3465e7fb.js"
+  }
+]);
